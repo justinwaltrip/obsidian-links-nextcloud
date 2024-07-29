6c3c998948e90e0e09e4ac8afaea01c7ff136493
Justin Waltrip <justin.waltrip@protonmail.com>
2024-07-29 17:59:16 -0400

    chore: Add devenv configuration files and update npm dependencies

--- commands/CreateLinkFromClipboardCommand.ts
+++ commands/CreateLinkFromClipboardCommand.ts
@@ -1,13 +1,85 @@
 import { Editor } from "obsidian";
-import { CommandBase, Func } from "./ICommand"
+import { CommandBase, Func } from "./ICommand";
 import { LinkTypes, findLink, findLinks, getFileName, getPageTitle, getSafeFilename } from "../utils";
 import { IObsidianProxy } from "./IObsidianProxy";
 import { selectWordUnderCursor } from "../editorUtils";
+import axios from 'axios';
+import { XMLParser } from 'fast-xml-parser';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+async function getFilePath(
+	fileId,
+	username = process.env.DAV_USERNAME || '',
+	password = process.env.DAV_PASSWORD || '',
+	url = 'https://nixos.tailc910f.ts.net/remote.php/dav/'
+) {
+	const data = `<?xml version="1.0" encoding="UTF-8"?>
+<d:searchrequest xmlns:d="DAV:" xmlns:oc="http://owncloud.org/ns">
+    <d:basicsearch>
+        <d:select>
+            <d:prop>
+                <d:displayname/>
+            </d:prop>
+        </d:select>
+        <d:from>
+            <d:scope>
+                <d:href>/files/admin</d:href>
+                <d:depth>infinity</d:depth>
+            </d:scope>
+        </d:from>
+        <d:where>
+            <d:eq>
+                <d:prop>
+                    <oc:fileid/>
+                </d:prop>
+                <d:literal>${fileId}</d:literal>
+            </d:eq>
+        </d:where>
+        <d:orderby/>
+    </d:basicsearch>
+</d:searchrequest>`;
+
+	const headers = { 'Content-Type': 'text/xml' };
+
+	try {
+		const response = await axios({
+			method: 'SEARCH',
+			url,
+			data,
+			headers,
+			auth: {
+				username,
+				password,
+			},
+		});
+
+		if (response.status !== 207) {
+			return null;
+		}
+
+		const parser = new XMLParser();
+		const parsedData = parser.parse(response.data);
+		const href = parsedData['d:multistatus']?.['d:response']?.['d:href'];
+
+		if (href) {
+			const fullPath = href as string;
+			if (fullPath.includes('/files/admin/')) {
+				const filePath = fullPath.split('/files/admin/')[1];
+				return filePath;
+			}
+		}
+
+		return null;
+	} catch (error) {
+		return null;
+	}
+}
 
 export class CreateLinkFromClipboardCommand extends CommandBase {
 	obsidianProxy: IObsidianProxy;
 	callback: ((error: Error | null, data: any) => void) | undefined
-
 
 	constructor(obsidianProxy: IObsidianProxy,
 		isPresentInContextMenu: Func<boolean> = () => true, isEnabled: Func<boolean> = () => true,
@@ -25,7 +97,7 @@ export class CreateLinkFromClipboardCommand extends CommandBase {
 		if (checking && !this.isEnabled()) {
 			return false;
 		}
-		// TODO: no check clipboard
+
 		if (checking) {
 			const noteText = editor.getValue();
 			const cursorOffset = editor.posToOffset(editor.getCursor('from'))
@@ -38,62 +110,37 @@ export class CreateLinkFromClipboardCommand extends CommandBase {
 
 		(async () => {
 			const httpUrlRegEx = /^(http|https):\/\/[^ "]+$/i;
-			// const linkDestination = await navigator.clipboard.readText();
-
 
 			const clipboardText = await this.obsidianProxy.clipboardReadText();
-			const links = findLinks(clipboardText, LinkTypes.All)
-			let linkText = "";
+			let linkDestination = clipboardText;
 
-			let linkDestination = "";
-			if (links.length) {
-				const link = links[0];
-				if (this.obsidianProxy.settings.ffObsidianUrlSupport && (link.type & LinkTypes.ObsidianUrl) === LinkTypes.ObsidianUrl) {
-					//TODO:
-					if (link.destination) {
-						const url = new URL(link.destination?.content)
-						if (this.obsidianProxy.Vault.getName() === url.searchParams.get('vault')) {
-							const filePath = url.searchParams.get('file')
-							if (filePath) {
-								linkDestination = decodeURI(filePath);
-								linkText = getFileName(linkDestination);
-							}
-						}
-					}
-				} else {
-					linkDestination = links[0].destination ? links[0].destination.content : ""
-				}
-			} else {
-				linkDestination = clipboardText;
+			if (!httpUrlRegEx.test(linkDestination)) {
+				return;
 			}
 
-			if (!linkText) {
-				linkText = linkDestination;
+			const url = new URL(linkDestination);
+			const pathSegments = url.pathname.split('/');
+			const fileId = pathSegments[pathSegments.length - 1];
+
+			if (!fileId) {
+				console.error('Invalid URL format');
+				return;
 			}
+
+			const filePath = await getFilePath(fileId);
+
+			if (!filePath) {
+				console.error('File path not found');
+				return;
+			}
+
+			const username = 'admin';
+			const newLink = `nextcloud://open-file?user=${username}&link=${url.origin}&path=${filePath}`;
 
 			let selection = editor.getSelection();
 
-			if (!selection
-				&& this.obsidianProxy.settings.autoselectWordOnCreateLink) {
+			if (!selection && this.obsidianProxy.settings.autoselectWordOnCreateLink) {
 				selection = selectWordUnderCursor(editor);
-			}
-
-			let isUrl = false;
-
-			if (selection.length == 0 && httpUrlRegEx.test(linkDestination)) {
-				isUrl = true;
-				const notice = this.obsidianProxy.createNotice("Getting title ...", 0);
-				try {
-					linkText = await getPageTitle(new URL(linkDestination), this.getPageText.bind(this));
-				}
-				catch (err) {
-					this.obsidianProxy.createNotice(err);
-					this.callback?.(err, undefined);
-					linkText = '';
-				}
-				finally {
-					notice.hide();
-				}
 			}
 
 			let posRangeStart = editor.getCursor();
@@ -101,20 +148,20 @@ export class CreateLinkFromClipboardCommand extends CommandBase {
 			if (selection.length > 0) {
 				posRangeStart = editor.getCursor('from');
 				posRangeEnd = editor.getCursor('to');
-				linkText = selection;
+				linkDestination = selection;
 			}
 
-			const requireAngleBrackets = !isUrl && linkDestination && linkDestination.indexOf(' ') > 0;
+			const requireAngleBrackets = newLink.indexOf(' ') > 0;
+			const linkRawText = requireAngleBrackets ? `[${linkDestination}](<${newLink}>)` : `[${linkDestination}](${newLink})`;
 
-			const linkRawText = requireAngleBrackets ? `[${linkText}](<${linkDestination}>)` : `[${linkText}](${linkDestination})`;
 			const endOffset = editor.posToOffset(posRangeStart) + linkRawText.length;
 			editor.replaceRange(linkRawText, posRangeStart, posRangeEnd);
-			if (linkText) {
+			if (linkDestination) {
 				editor.setCursor(editor.offsetToPos(endOffset));
 			} else {
 				editor.setCursor(editor.offsetToPos(editor.posToOffset(posRangeStart) + 1));
 			}
-			this.callback?.(null, undefined)
+			this.callback?.(null, undefined);
 		})();
 	}
 
